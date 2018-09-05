d904c73156001ffa9c0c77c8180ca789dd8a9309
juancarlosvim <juancarlosvillegasmarmol@gmail.com>
2018-09-05 09:44:03 +0200

    Peticiones HTTP en angular: introducción

--- spotiLite/src/app/components/home/home.component.ts
+++ spotiLite/src/app/components/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  paises: any[] = [];
+  constructor(private http: HttpClient) {
+    this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe( (resp: any) =>{
+        this.paises = resp;
+        console.log(resp);
+    });
+  }
 
   ngOnInit() {
   }
